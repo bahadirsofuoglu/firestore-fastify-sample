e7fc485906b7c7a124e73f6d89f8b87907e9d8bb
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-04 17:42:20 +0300

    user controller changes

--- controllers/user.js
+++ controllers/user.js
@@ -4,32 +4,18 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 exports.signUp = async (req, res) => {
+  const { firstname, lastname, email } = req.body
   const hashPassword = await bcrypt.hash(req.body.password, saltRounds)
   const userDetail = {
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
+    firstname,
+    lastname,
+    email,
     password: hashPassword
   }
-  await firebase
-    .auth()
-    .createUserWithEmailAndPassword(userDetail.email, userDetail.password)
+  await firebase.auth().createUserWithEmailAndPassword(email, hashPassword)
   await db
     .collection('user')
     .doc(firebase.auth().currentUser.uid)
     .set(userDetail)
   res.send(userDetail)
-  /*   try {
-    const userData = { email: req.body.email, password: hash }
-    admin
-      .auth()
-      .createUserWithEmailAndPassword(userData.email, userData.password)
-    db.collection('user')
-      .doc()
-      .set(userData)
-    res.send({ message: 'User Created' })
-  } catch (e) {
-    console.log(e)
-    res.send({ message: 'Error creating user' })
-  } */
 }
